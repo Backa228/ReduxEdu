c2692f466d97de63ec6bbcbd0336923323b273ea
Backa228 <andrey.scott01@gmail.com>
2026-02-03 17:07:08 +0200

    lesson 03.02
    Зробили підписку на стор для компонентів TaskList, TaskCounter. налаштували фільтр списку та щоб Counter рахував кількість виконаних/невиконаних завдань.

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,12 +2,30 @@ import { Task } from '../Task/Task';
 import css from './TaskList.module.scss';
 import { useSelector } from 'react-redux'
 
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case 'active':
+      return tasks.filter((task) => !task.completed)
+    case 'completed':
+      return tasks.filter((task) => task.completed)
+    default:
+      return tasks
+  }
+}
+
 export const TaskList = () => {
   const tasks = useSelector(state => state.tasks.items);
 
+  const statusFilter = useSelector(state => state.filters.status)
+
+  console.log('Current status filter:', statusFilter);
+  console.log('Tasks from state:', tasks);
+
+  const visibleTasks = getVisibleTasks(tasks, statusFilter)
+
   return (
     <ul className={css.list}>
-      {tasks.map((task) => (
+      {visibleTasks.map((task) => (
         <li className={css.listItem} key={task.id}>
           <Task task={task} />
         </li>
