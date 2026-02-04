8545fe46c5378b6e173a1bc44b395742a04f82b3
Backa228 <andrey.scott01@gmail.com>
2026-02-04 17:32:23 +0200

    lesson 04.02
    Розібрали тему "Екшени", створили екшени для додавання, видалення та перемикання задач. Потім підключили ці екшени до компонентів TaskForm (для додавання задач) та Task (для видалення та перемикання задач).

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,19 @@
 import { Button } from '../Button/Button';
 import css from './TaskForm.module.scss';
+import { useDispatch } from 'react-redux'
+import { addTask } from '../../redux/actions';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch()
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    dispatch(addTask({
+      id: Date.now(),
+      text: form.elements.text.value,
+      completed: false,
+    }))
     form.reset();
   };
 
---
+++ src/redux/actions.js
@@ -0,0 +1,34 @@
+// export const addTask = newTask => {
+//     return {
+//         type: 'tasks/addTask',
+//         payload: newTask
+//     }
+// }
+
+// export const deleteTask = taskID => {
+//     return {
+//         type: 'tasks/deleteTask',
+//         payload: taskID
+//     }
+// }
+
+// export const toggleTask = taskID => {
+//     return {
+//         type: 'tasks/toggleTask',
+//         payload: taskID,
+//     }
+// }
+
+// export const setStatusFilter = taskID => {
+//     return {
+//         type: 'tasks/toggleTask',
+//         payload: taskID,
+//     }
+// }
+
+import { createAction } from "@reduxjs/toolkit";
+
+export const addTask = createAction('tasks/addTask');
+export const deleteTask = createAction('tasks/deleteTask');
+export const toggleTask = createAction('tasks/toggleTask');
+export const setStatusFilter = createAction('filters/setStatusFilter');
